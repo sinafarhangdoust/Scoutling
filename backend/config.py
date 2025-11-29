8bb6d656f7a8dbac311f34ea4dbec7aa9dd374f2
Sina Farhang Doust <sina.farhangdoust@gmail.com>
2025-11-29 18:07:06 +0100

    feat: updated the env variables handling in a dynamic way

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,16 @@
-import os
+from pathlib import Path
 import logging
+
+from dotenv import dotenv_values
+
+dev_env_path = env_path = (Path(__file__).resolve().parent / ".." / ".env.development").resolve()
+config = dotenv_values(dotenv_path=dev_env_path)
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 try:
-    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+    OPENAI_API_KEY = config['OPENAI_API_KEY']
 except KeyError:
     raise KeyError('OPENAI_API_KEY not set!')
 
